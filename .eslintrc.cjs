bd0dedd8325840dc96929efaa650448f0982f54b
warugaki <warugaki.web.developer@gmail.com>
2022-01-08 16:20:41 +0900

    Eslintのルール無効化

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -37,6 +37,7 @@ module.exports = {
       rules: {
         'import/no-mutable-exports': 'off',
         'import/prefer-default-export': 'off',
+        'import/first': 'off',
       },
     },
   ],
